813eb54e4a0474d29b2482cc35a4a638480f3256
Oursin <Oursin@users.noreply.github.com>
2020-01-21 08:56:56 +0000

    Automated deployment: Tue Jan 21 08:56:56 UTC 2020 6b0d170106f7966ffb0c9b82faa0e52fce326e07

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['imodule',['IModule',['../classIModule.html',1,'']]],
+  ['imodule',['IModule',['../classZia_1_1API_1_1IModule.html',1,'Zia::API']]],
   ['irequest',['IRequest',['../classZia_1_1API_1_1HTTP_1_1IRequest.html',1,'Zia::API::HTTP']]],
   ['iresponse',['IResponse',['../classZia_1_1API_1_1HTTP_1_1IResponse.html',1,'Zia::API::HTTP']]]
 ];
