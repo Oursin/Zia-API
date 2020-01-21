7efa0b2d3e71b6960c16127b21270d444efb4b07
Maxime CORBIN <maxime.corbin@epitech.eu>
2020-01-21 15:55:32 +0100

    More doc, improved Status code for compatibilty with MSVC++

--- module.hpp
+++ module.hpp
@@ -1,24 +1,52 @@
 #pragma once
 
 #include <variant>
-#include <list>
+#include <vector>
 #include "request.hpp"
 #include "response.hpp"
 
 namespace Zia::API {
 
-using ConfigValue = std::variant<std::string, std::list<std::string>, int, bool>;
+using ConfigValue = std::variant<std::string, std::vector<std::string>, int, bool>;
 
 using Config = std::map<std::string, ConfigValue>;
 
 enum ExecState {
-    Before, // Before/While request
-    After, // After request
+    /**
+     * Right after te request is received
+     */
+    Before,
+    /**
+     * While request is handled
+     */
+    During,
+    /**
+     * Right before the response is sent
+     */
+    Last,
+    /**
+     * After the response is sent back
+     */
+    After,
 };
 
 class IModule {
-    virtual enum ExecState when() = 0; // should return an ExecState to know when the module should be loaded
-    virtual void exec(HTTP::IRequest &request, HTTP::IResponse &response) = 0;
+    /**
+     * When represents the steps during request handling when the module expects to be called.
+     * @return A vector of ExecState values
+     */
+    virtual std::vector<enum ExecState> when() = 0;
+    /**
+     * Exec is the main method of a module. It will be called by the core whenever the core decides to invoke the module.
+     * @param request The Request object
+     * @param response The Response object
+     * @param when The time during execution where this module was called
+     */
+    virtual void exec(HTTP::IRequest &request, HTTP::IResponse &response, enum ExecState when) = 0;
+    /**
+     * Lets the module store it's config.
+     * @param config A map of every config parameters
+     */
     virtual void loadConfig(Config &config) = 0;
 };
 
--- request.hpp
+++ request.hpp
@@ -3,30 +3,50 @@
 #include <any>
 #include <string>
 #include <map>
+#include "methods.hpp"
 
 namespace Zia::API::HTTP {
 class IRequest {
     /**
-     *
-     * @return
+     * Gets the raw bytes of a request
+     * @return The string that represents the request
      */
-    virtual std::any body() = 0; // Body should be a string at first ? Can be changed by modules, like images or json object
+    virtual std::string getRaw() = 0;
     /**
-     *
-     * @param field
-     * @return
+     * Sets the raw bytes of the request.
+     * This method should only be used by the Core and any module that deciphers the contents of the request.
+     */
+    virtual void setRqw(std::string raw) = 0;
+    /**
+     * Parses the request from it's raw bytes.
+     * This method should at least fill the headers and the url parameters.
+     */
+     virtual void parse() = 0;
+    /**
+     * Returns the body of the request, as parsed by any body parsing module used.
+     * @return The body of the request
+     */
+    virtual std::any body() = 0;
+    /**
+     * Gets the value of a header field
+     * @param field The name of the field to get
+     * @return The value of the field, an empty string if no such field is found.
      */
     virtual std::string getHeader(std::string field) = 0;
-    virtual std::string hostname() = 0;
-    virtual std::string ip() = 0; // 1.1.1.1
-    virtual std::string method() = 0; // GET
-    virtual std::string protocol() = 0; // HTTP/1.1
-    virtual std::string url() = 0; // http://site.tld/page/info?query
-
-    virtual bool accept(std::string mimetype) = 0;
-    virtual bool acceptCharset(std::string charset) = 0;
-    virtual bool acceptEncoding(std::string encoding) = 0;
-    virtual bool acceptLanguage(std::string language) = 0;
-    virtual bool isType(std::string mimeType) = 0;
+    /**
+     * Returns the method of the request
+     * @return The HTTP::Method object that corresponds to the method for this request
+     */
+    virtual HTTP::Method method() = 0;
+    /**
+     * Returns the protocol string for the request, e.g. HTTP/1.1
+     * @return The protocol string
+     */
+    virtual std::string protocol() = 0;
+    /**
+     * The full url of the request, e.g. http://site.tld/page/info?query
+     * @return The url string
+     */
+    virtual std::string url() = 0;
 };
 }
