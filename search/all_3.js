03957cbfb1d4bb2203651304885e06153dbdd5b0
gabrielcolson <gabrielcolson@users.noreply.github.com>
2020-01-26 18:04:37 +0000

    Automated deployment: Sun Jan 26 18:04:37 UTC 2020 bc9e29895216d371dad11b36402ddb6012d58ba0

--- search/all_3.js
+++ search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getheader',['getHeader',['../classzia_1_1http_1_1Request.html#acc7db1c5a403c02ce05100d7c230ee64',1,'zia::http::Request::getHeader()'],['../classzia_1_1http_1_1Response.html#a568535d71b6f745f938470859483eb5d',1,'zia::http::Response::getHeader()']]],
+  ['getheader',['getHeader',['../classzia_1_1http_1_1Request.html#acc7db1c5a403c02ce05100d7c230ee64',1,'zia::http::Request::getHeader()'],['../classzia_1_1http_1_1Response.html#a96ddcd5055d3870d9810fc2a69824242',1,'zia::http::Response::getHeader()']]],
   ['getraw',['getRaw',['../classzia_1_1http_1_1Request.html#a8a675a65b00795f4308639500901a0ba',1,'zia::http::Request']]],
   ['getstatus',['getStatus',['../classzia_1_1http_1_1Response.html#aa16e9370a30869148380f3fbf04d594b',1,'zia::http::Response']]]
 ];
