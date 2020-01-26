612d73000b7b62e7d718f6cf51b453f0c453b137
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-01-26 18:14:16 +0100

    add chaining to Response setters

--- Response.hpp
+++ Response.hpp
@@ -25,13 +25,13 @@ public:
      * @param cookie The name of the cookie to set
      * @param value The value of the cookie
      */
-    virtual void setCookie(std::string cookie, std::string value) = 0;
+    virtual Response &setCookie(std::string cookie, std::string value) = 0;
 
     /**
      * Delete a cookie from the Set-Cookie header, as defined in RFC 6265
      * @param cookie The name of the cookie to delete
      */
-    virtual void clearCookie(std::string cookie) = 0;
+    virtual Response &clearCookie(std::string cookie) = 0;
 
     /**
      * Returns the value of a header field, as defined in RFC 2616, 4.2
@@ -45,7 +45,7 @@ public:
      * @param field The field to set
      * @param value The value to associate with this header field
      */
-    virtual void setHeader(std::string field, std::string value) = 0;
+    virtual Response &setHeader(std::string field, std::string value) = 0;
 
     /**
      * Gets the current status code set for the response, as defined in RFC 2616, 6.1
@@ -57,18 +57,18 @@ public:
      * Sets the status code for the response, as defined in RFC 2616, 6.1
      * @param statusCode The status code to set
      */
-    virtual void setStatus(http::Status statusCode) = 0;
+    virtual Response &setStatus(http::Status statusCode) = 0;
 
     /**
      * Sets a file as the body of the reponse
      * @param filePath the file's path
      */
-    virtual void setFile(std::string filePath) = 0;
+    virtual Response &setFile(std::string filePath) = 0;
 
     /**
      * Sets the body of the response from a file
      * @param body The body of the request
      */
-    virtual void setBody(std::string body) = 0;
+    virtual Response &setBody(std::string body) = 0;
 };
 }
